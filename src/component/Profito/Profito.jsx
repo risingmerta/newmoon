e1f13888d539e9b0dd38e4278092492d750d7d9c
rishabh-star <risingmerta@gmail.com>
2025-04-16 16:33:44 +0530

    first commit

--- src/component/Profito/Profito.jsx
+++ src/component/Profito/Profito.jsx
@@ -1,11 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { FaKey, FaPen, FaUser } from "react-icons/fa";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { imageData } from "@/data/imageData";
-// import { signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import "./profito.css";
-import SignInSignUpModal from "../SignSignup/SignInSignUpModal";
 
 export default function Profito() {
   const { data: session } = useSession();
@@ -14,7 +13,6 @@ export default function Profito() {
   const [newPassword, setNewPassword] = useState("");
   const [newAvatar, setNewAvatar] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [showMod, setShowMod] = useState(false);
 
   useEffect(() => {
     if (session?.user) {
@@ -28,11 +26,6 @@ export default function Profito() {
   const formattedDate = `${date.getDate()}-${date.toLocaleString("default", {
     month: "long",
   })}-${date.getFullYear()}`;
-
-  const [logIsOpen, setLogIsOpen] = useState(false);
-  const sign = (sign) => {
-    setLogIsOpen(sign);
-  };
 
   const handleSave = async () => {
     const userId = session?.user?.id;
@@ -62,7 +55,14 @@ export default function Profito() {
     const data = await response.json();
 
     if (response.ok) {
-      await signOut({ redirect: false });
+      if (updatedFields.email || updatedFields.password) {
+        await signIn("credentials", {
+          email: newEmail,
+          password: newPassword || "",
+          redirect: false,
+        });
+      }
+
       alert("Profile updated successfully");
       setShowModal(false);
     } else {
@@ -71,121 +71,112 @@ export default function Profito() {
   };
 
   return (
-    <>
-      {!session && (
-        <SignInSignUpModal
-          logIsOpen={logIsOpen}
-          setLogIsOpen={setLogIsOpen}
-          sign={sign}
-        />
-      )}
-      <div className="comAll">
-        <div className="profile-header">
-          <FaUser /> Edit Profile
-        </div>
-        <div className="profile-content">
-          <div className="cofs">
-            <div className={`rofile-image`}>
-              <img
-                src={newAvatar || session?.user?.avatar}
-                className="profile-img"
-                alt="Profile"
-              />
-              <div className="cof-pen" onClick={() => setShowModal(true)}>
-                <FaPen />
-              </div>
-            </div>
-          </div>
-
-          <div className="profile-details">
-            <div className="profile-field">
-              <div className="field-label">EMAIL ADDRESS</div>
-              <input
-                className="field-input"
-                type="text"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)}
-                name="email"
-              />
-            </div>
-            <div className="profile-field">
-              <div className="field-label">YOUR NAME</div>
-              <input
-                className="field-input"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewUsername(e.target.value)}
-                name="name"
-              />
-            </div>
-            <div className="profile-field">
-              <div className="field-label">JOINED</div>
-              <div className="field-value">{formattedDate}</div>
-            </div>
-            <div className="paske">
-              <FaKey /> Change Password
-            </div>
-            <div className="profile-field">
-              <div className="field-label">NEW PASSWORD</div>
-              <input
-                className="field-input"
-                type="password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                name="password"
-                placeholder="Leave empty to keep current password"
-              />
-            </div>
-            <div className="save-button" onClick={handleSave}>
-              Save
-            </div>
-          </div>
-          <div className="cofs">
-            <div className={`profile-image`}>
-              <img
-                src={newAvatar || session?.user?.avatar}
-                className="profile-img"
-                alt="Profile"
-              />
-              <div className="cof-pen" onClick={() => setShowModal(true)}>
-                <FaPen />
-              </div>
-            </div>
-          </div>
-
-          {showModal && (
-            <div className="avatar-modal">
-              <div className="modal-content">
-                <h3>Select an Avatar</h3>
-                <div className="avatar-selection">
-                  {Object.keys(imageData.hashtags).map((category) => (
-                    <div key={category} className="avatar-category">
-                      <h4>{category}</h4>
-                      <div className="avatar-images">
-                        {imageData.hashtags[category].images.map((img, idx) => (
-                          <img
-                            key={idx}
-                            src={img}
-                            alt={category}
-                            onClick={() => setNewAvatar(img)}
-                            className="avatar-image"
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="modal-footer">
-                  <button onClick={() => setShowModal(false)}>Close</button>
-                  <button onClick={() => handleSave() & setShowModal(false)}>
-                    Save
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
+    <div className="comAll">
+      <div className="profile-header">
+        <FaUser /> Edit Profile
       </div>
-    </>
+      <div className="profile-content">
+        <div className="cofs">
+          <div className={`rofile-image`}>
+            <img
+              src={newAvatar || session?.user?.avatar}
+              className="profile-img"
+              alt="Profile"
+            />
+            <div className="cof-pen" onClick={() => setShowModal(true)}>
+              <FaPen />
+            </div>
+          </div>
+        </div>
+
+        <div className="profile-details">
+          <div className="profile-field">
+            <div className="field-label">EMAIL ADDRESS</div>
+            <input
+              className="field-input"
+              type="text"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+              name="email"
+            />
+          </div>
+          <div className="profile-field">
+            <div className="field-label">YOUR NAME</div>
+            <input
+              className="field-input"
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              name="name"
+            />
+          </div>
+          <div className="profile-field">
+            <div className="field-label">JOINED</div>
+            <div className="field-value">{formattedDate}</div>
+          </div>
+          <div className="paske">
+            <FaKey /> Change Password
+          </div>
+          <div className="profile-field">
+            <div className="field-label">NEW PASSWORD</div>
+            <input
+              className="field-input"
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              name="password"
+              placeholder="Leave empty to keep current password"
+            />
+          </div>
+          <div className="save-button" onClick={handleSave}>
+            Save
+          </div>
+        </div>
+        <div className="cofs">
+          <div className={`profile-image`}>
+            <img
+              src={newAvatar || session?.user?.avatar}
+              className="profile-img"
+              alt="Profile"
+            />
+            <div className="cof-pen" onClick={() => setShowModal(true)}>
+              <FaPen />
+            </div>
+          </div>
+        </div>
+
+        {showModal && (
+          <div className="avatar-modal">
+            <div className="modal-content">
+              <h3>Select an Avatar</h3>
+              <div className="avatar-selection">
+                {Object.keys(imageData.hashtags).map((category) => (
+                  <div key={category} className="avatar-category">
+                    <h4>{category}</h4>
+                    <div className="avatar-images">
+                      {imageData.hashtags[category].images.map((img, idx) => (
+                        <img
+                          key={idx}
+                          src={img}
+                          alt={category}
+                          onClick={() => setNewAvatar(img)}
+                          className="avatar-image"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="modal-footer">
+                <button onClick={() => setShowModal(false)}>Close</button>
+                <button onClick={() => handleSave() & setShowModal(false)}>
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
