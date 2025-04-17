163117d93bea1d166ddd90df15c5389d631859d9
rishabh-star <risingmerta@gmail.com>
2025-04-17 13:02:54 +0530

    first commit

--- src/app/ad/page.jsx
+++ src/app/ad/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
         src="//disgustingmad.com/b29918b4e5fbf3e4c13e32f24c7c143c/invoke.js"
         strategy="afterInteractive"
         async
-        data-cfasync="false"
+        // data-cfasync="false"
       />
       {/* Ad container */}
       <div id="container-b29918b4e5fbf3e4c13e32f24c7c143c"></div>
