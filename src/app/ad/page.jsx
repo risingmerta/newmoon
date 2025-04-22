623a66af09bf7a8427bf97aebf17479dafa7b2b6
rishabh-star <risingmerta@gmail.com>
2025-04-22 05:52:23 +0530

    first commit

--- src/app/ad/page.jsx
+++ src/app/ad/page.jsx
@@ -5,8 +5,9 @@ const Page = () => {
   return (
     <div>
       <Script
-        src="//forecastcremateillogical.com/b29918b4e5fbf3e4c13e32f24c7c143c/invoke.js"
+        src="//abackdamstubborn.com/b29918b4e5fbf3e4c13e32f24c7c143c/invoke.js"
         strategy="afterInteractive"
+        data-cfasync="false"
         async
       />
       <div id="container-b29918b4e5fbf3e4c13e32f24c7c143c" />
--- src/app/ad3/page.jsx
+++ src/app/ad3/page.jsx
@@ -5,9 +5,10 @@ const Page = () => {
   return (
     <div>
       <Script
-        src="//forecastcremateillogical.com/a8364aeaeff99fac8326528d16cd0941/invoke.js"
+        src="//abackdamstubborn.com/a8364aeaeff99fac8326528d16cd0941/invoke.js"
         strategy="afterInteractive"
         async
+        data-cfasync="false"
       />
       <div id="container-a8364aeaeff99fac8326528d16cd0941" />
     </div>
