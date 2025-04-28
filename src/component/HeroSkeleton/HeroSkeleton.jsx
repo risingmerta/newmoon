8f89115fcbb0534d96457a8b2477078f97a4ab9d
rishabh-star <risingmerta@gmail.com>
2025-04-28 18:17:45 +0530

    first commit

---
+++ src/component/HeroSkeleton/HeroSkeleton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HeroSkeleton.css'; // Import the CSS file
+
+const HeroSkeleton = () => {
+  return (
+    <div className="hero-skeleton"></div>
+  );
+};
+
+export default HeroSkeleton;
